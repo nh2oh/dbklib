aaa77967aff38526f251c357cbb20f18316b11d3
ben knowles <ben_junk@ionophore.com>
2018-11-22 19:10:05 -0600

    iterseq into proj

--- dbklib/iterseq.h
+++ dbklib/iterseq.h
@@ -98,50 +98,5 @@ int print_recursive(tree_t<N_tree,N_node> tree, std::array<int,2> cpos, std::str
 }
 
 
-/*
-
-	while (!stk_taddr.empty()) {
-		cpos = stk_taddr.top();
-		stk_taddr.pop();
-
-		for (int i=0; i<(N_tree-cpos[0]); ++i) {
-			if (vs.size() == 0) { break; }  // Occurs only on the first iteration
-			vs.pop_back();
-		}
-
-		// On entering this inner loop, cpos[0] takes its value from whatever was popped
-		// from the stack, however, on subsequent iterations cpos[0] is set to 0 (see bottom
-		// of loop).  
-		// TODO:  Would the loop be simpler if instead i pushed the "address" of the present
-		// node and let the loop calculate the next?
-		for (true; cpos[0]<N_tree; ++cpos[0]) {
-			// Node processing
-			if (tree.pos[cpos[0]].prob[cpos[1]] != 0) {
-				s = "{" + std::to_string(cpos[0]) + "," + std::to_string(cpos[1]) + "}=" 
-					+ std::to_string(tree.pos[cpos[0]].prob[cpos[1]]) + "->";
-			} else {
-				s = " ";
-			}
-			vs.push_back(s);
-
-			if (cpos[1] < (N_node-1)) {
-				// {cpos[0],cpos[1]+1} is the "address" of the next node connected
-				// to the present node; in a breadth-first traversal, this is where we
-				// would move to next.  
-				stk_taddr.push({cpos[0],cpos[1]+1});
-			}
-
-			cpos[1]=0;  // On next tree node, want to start @ 0
-		}
-
-		for (const auto& e : vs) {
-			std::cout << e;
-		}
-		std::cout << std::endl;
-	}
-
-	return 0;
-}*/
-
 
 
