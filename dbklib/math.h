737b116c1c38d48f781adb5affcb713a64a184fa
ben knowles <ben_junk@ionophore.com>
2018-12-23 16:55:42 -0600

    vprod()

--- dbklib/math.h
+++ dbklib/math.h
@@ -29,6 +29,30 @@ std::vector<std::vector<T>> transpose(const std::vector<std::vector<T>>& m) {
 }
 
 
+template<typename Tf, typename Ts>
+std::vector<Tf> vprod(std::vector<Tf> a, std::vector<Ts> b) {
+	if (a.size() != b.size()) {
+		std::abort();
+	}
+	std::vector<Tf> res {};  res.reserve(a.size());
+	for (size_t i=0; i<a.size(); ++i) {
+		res.push_back(a[i]*b[i]);
+	}
+	return res;
+}
+
+template<typename Tf, typename Ts, typename... Trest>
+std::vector<Tf> vprod(std::vector<Tf> a, std::vector<Ts> b, Trest... rest) {
+	if (a.size() != b.size()) {
+		std::abort();
+	}
+	std::vector<Tf> res {};  res.reserve(a.size());
+	for (size_t i=0; i<a.size(); ++i) {
+		res.push_back(a[i]*b[i]);
+	}
+	return vprod(res,rest...);
+}
+
 
 // Kahan summation
 template<typename T>
