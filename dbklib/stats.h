f7073fbeb81b9ee73dd842d85f53784d8ae60722
ben knowles <ben_junk@ionophore.com>
2018-12-23 18:15:11 -0600

    rsample()

--- dbklib/stats.h
+++ dbklib/stats.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <vector>
+#include <random>
+#include <iterator>
+
+
 
 struct linreg_result {
 	bool result_valid {false};
@@ -12,5 +16,31 @@ linreg_result linreg(const std::vector<double>, const std::vector<double>&);
 double mean(const std::vector<double>&);
 double corr(const std::vector<double>&, const std::vector<double>&);
 std::vector<double> corr(const std::vector<double>&, const std::vector<std::vector<double>>&);
+
+
+//
+// Copy N randomly selected elements from the range [beg,end) into dest.  Essentially identical
+// to std::sample(), however, std::sample() copies a maximum of end-beg elements and never 
+// selects the same element twice.  rsample() may select the same element multiple times.  
+//
+template<typename T>
+void rsample(typename std::vector<T>::const_iterator beg,
+	typename std::vector<T>::const_iterator end, typename std::vector<T>& dest,
+	size_t N, std::mt19937& re) {
+	size_t max = static_cast<size_t>(end-beg-1);
+	if (max <= 0) {
+		std::abort();  // attempting to sample an empty population
+	}
+	
+	std::uniform_int_distribution<size_t> rd {0,max};
+	for (size_t i=0; i<N; ++i) {
+		std::back_inserter(dest) = *(beg + rd(re));
+	}
+};
+
+
+
+
+
 
 
